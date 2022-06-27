e9468cbf8fa04680f5a2ff54708d7135d063c702
Jerry Rivas <kiroto50@gmail.com>
2022-06-27 19:37:37 -0400

    Version bump

--- 004 - Kata Range/setup.py
+++ 004 - Kata Range/setup.py
@@ -5,6 +5,6 @@ setup(
     author="Jerry Rivas",
     author_email="jerry.rivas@hotmail.com",
     description="A package that manages ranges and extremes",
-    version="1.1",
+    version="1.2",
     packages=["KRange"]
 )
