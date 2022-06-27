8df7ebf79055f6b00a40f6d8bd61845ffdc023bf
Jerry Rivas <kiroto50@gmail.com>
2022-06-27 18:56:33 -0400

    Get user input data from ranges

--- 005 - Kata Range Main/main.py
+++ 005 - Kata Range Main/main.py
@@ -1,10 +1,25 @@
+import os
 from KRange.range import Range
 
-def exitFunct():
+def exitFunct(**kwargs):
     return -1
 
-def enterFunct():
-    return 0
+def enterFunct(**kwargs):
+    ranges = kwargs["ranges"]
+    print("Enter a new range")
+    print("Ranges of the form: <( or [> + <integer> + \",\" + <integer> + <) or ]> ")
+    usrIn = input()
+    firstBracket = usrIn[0]
+    lastBracket = usrIn[-1]
+    division = usrIn.find(",")
+    firstNumber = usrIn[1: division]
+    secondNumber = usrIn[division + 1: -1]
+
+    print("FB: ", firstBracket)
+    print("LB: ", lastBracket)
+    print("DIV: ", division)
+    print("FN: ", firstNumber)
+    print("SN: ", secondNumber)
 
 functions = {
     "exit": exitFunct,
@@ -12,16 +27,29 @@ functions = {
 }
 
 
+def showFunctions(**kwargs):
+    keys = functions.keys()
+    print("The available commands are:")
+    for key in keys:
+        print(key)
+
+
+functions["help"] = showFunctions
+
 if __name__ == "__main__":
+    print("Welcome to the Range Testing program!")
+    rangeList = []
+    showFunctions()
     while(True):
+        print("Issue your next command")
         usrIn = input().lower()
         toExecute = functions[usrIn]
-        
-        if (usrIn in functions.keys):
+        os.system("cls")
+
+        if (usrIn in functions.keys()):
             toExecute = functions[usrIn]
-            if (toExecute() == -1):
+            if (toExecute(ranges= rangeList) == -1):
                 break
             # program
 
-        
 
