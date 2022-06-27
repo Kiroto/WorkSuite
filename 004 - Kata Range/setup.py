f5d68bed0c6bb91432f4c68dbfd9dd8696030159
Jerry Rivas <kiroto50@gmail.com>
2022-06-27 18:24:34 -0400

    Make Distributable code

---
+++ 004 - Kata Range/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="KRange",
+    author="Jerry Rivas",
+    author_email="jerry.rivas@hotmail.com",
+    description="A package that manages ranges and extremes",
+    version="1.0",
+    packages=["KRange"]
+)
