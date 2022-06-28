9ba9e3d2575d956f4e50e2f5bbdf66acb55967c2
Jerry Rivas <kiroto50@gmail.com>
2022-06-27 20:25:08 -0400

    Better error handling on overlap check and range list related

--- 005 - Kata Range Main/main.py
+++ 005 - Kata Range Main/main.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentError
 import os
 from unicodedata import numeric
 from KRange.range import Range
@@ -39,61 +40,75 @@ def enterFunct(**kwargs):
     print(f"Successfully added the range {firstBracket}{firstNumber}, {lastNumber}{lastBracket}!")
 
 def listFunct(**kwargs):
+    print("Available Ranges:")
     ranges = kwargs["ranges"]
     en = enumerate(ranges)
     for idx, range in en:
         print(f"{idx} - {range}")
 
+def getValidIndex(ranges):
+    rangeIndexText = input()
+    if (not rangeIndexText.isnumeric()):
+        raise ArgumentError("Argument is not numeric")
+    rangeIndex = int(rangeIndexText)
+    if(not (rangeIndex >= 0 and rangeIndex < len(ranges))):
+        raise IndexError("Index out of range")
+    return rangeIndex
+
+
 def delFunct(**kwargs):
     ranges = kwargs["ranges"]
+    if (len(ranges) == 0):
+        print("There are no ranges to remove.")
+        return
     listFunct(ranges = ranges)
-    print("what range do you whant to remove?: ")
-    delText = input()
-    if (not delText.isnumeric()):
+    print(f"What range do you whant to remove? (0-{len(ranges)})")
+
+    try:
+        delIndex = getValidIndex(ranges)
+        ranges.pop(delIndex)
+    except(ArgumentError):
         print("the index that you're searching was not a number. please write a valid index")
         return
-    delIndex = int(delText)
-    if(not (delIndex >= 0 and delIndex < len(ranges))):
-        print("the index you are searching is not available, please cry about it :)")
-        return 
-    ranges.pop(delIndex)
-    
+    except(IndexError):
+        print("the index you are searching is out of range, please cry about it :)")
+        return
+
+
 def overlapFunct(**kwargs):
     ranges = kwargs["ranges"]
-
-    idxText = input()
-    if (not idxText.isnumeric()):
-        print("the index that you're searching was not a number. please write a valid index")
+    if (len(ranges) == 0):
+        print("There are no ranges to check for overlap.")
         return
-    delIndex = int(idxText)
-    if(not (delIndex >= 0 and delIndex < len(ranges))):
-        print("the index you are searching is not available, please cry about it :)")
+    listFunct(ranges = ranges)
+
+    try:
+        print("Insert the first range")
+        firstIndex = getValidIndex(ranges)
+        r1 = ranges[firstIndex]
+        print(r1)
+        print("Insert the second range")
+        secondIndex = getValidIndex(ranges)
+        r2 = ranges[secondIndex]
+        print(r2)
+        print(f"The ranges  {r1} and {r2}", end=" ")
+        if (r1.overlaps(r2)):
+            print("do overlap")
+        else:
+            print("do not overlap.")
+    except(ArgumentError):
+        print("The index that you're searching was not a number. Please write a valid index")
         return
-    
-    r1 = ranges [idxText]
-
-    idxText = input()
-    if (not idxText.isnumeric()):
-        print("the index that you're searching was not a number. please write a valid index")
+    except(IndexError):
+        print("The index you are searching is out of range. Please cry about it :( 😿")
         return
-    delIndex = int(idxText)
-    if(not (delIndex >= 0 and delIndex < len(ranges))):
-        print("the index you are searching is not available, please cry about it :)")
-        return
-
-    r2 = ranges [idxText]
-    
-    print(r1.overlaps(r2))
-    
-
-
 
 functions = {
     "exit": exitFunct,
     "enter": enterFunct,
     "list": listFunct,
     "delete": delFunct,
-    "overlapCheck": overlapFunct
+    "overlapcheck": overlapFunct
 }
 
 
@@ -111,7 +126,7 @@ if __name__ == "__main__":
     rangeList = []
     showFunctions()
     while(True):
-        print("Issue your next command")
+        print("Issue your next command (or help)")
         usrIn = input().lower()
         os.system("cls")
 
@@ -120,5 +135,7 @@ if __name__ == "__main__":
             if (toExecute(ranges= rangeList) == -1):
                 break
             # program
+        else:
+            print(f"I did not understand \"{usrIn}\"")
 
 
